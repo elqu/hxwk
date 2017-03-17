74f5e616d055d20588757545fe144e8799f74a8c
Bent Fuhlrott <bent@fuhlrott.me>
2017-03-17 21:54:24 +0100

    Added command line argument support

--- main.cpp
+++ main.cpp
@@ -6,10 +6,18 @@
 #include "VisitorPattern.hpp"
 #include "llvm/ADT/StringRef.h"
 #include <cstdio>
+#include <iostream>
 #include <memory>
 #include <string>
 
-int main() {
+static void show_usage() {
+    std::cerr << "Usage: " << argv[0] << " [option(s)] VALUES"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << std::endl;
+}
+
+int main(int argc, char** argv) {
     Parser par{Lexer{}};
     IRGenerator gen{"Hexenwerk"};
     IRStatementVis vis_code{gen};
@@ -25,7 +33,14 @@ int main() {
         }
     }
 
-    gen.print();
+    gen.print();    
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || ("arg == "--help")) {
+            show_usage();
+        }
+    }
 
     return 0;
 }
