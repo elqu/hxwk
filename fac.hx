a3bc365276d12d26b3d7bbf7254846b49085a64a
rasburg <bent@fuhlrott.me>
2017-07-03 21:17:47 +0200

    Add example Hexenwerk program
    A simple exemplary implementation of the faculty function in Hexenwerk.

---
+++ fac.hx
@@ -0,0 +1,12 @@
+fn fac(a: i32) -> i32 {
+    if a < 2 {
+        1
+    } else {
+        a * fac(a - 1)
+    }
+} 
+
+fn main() -> void {
+    let x = 6;
+    printf("Die Fakultaet von %d ist %d.\n", x, fac(x));
+}
